f18fa41c4a79ddb88c949bd3ecf51b65bdf582da
ArtusAbelard <tribelha@gmail.com>
2023-11-14 09:49:11 +0100

    exo html

---
+++ structure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ mkdir -p public/{css,js,img,page}
+ touch public/js/main.js
+ touch index.html
+ touch public/css/style.css
+ 
+ echo"<!DOCTYPE html>
+ <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+ </body>
+ </html>">$Script/index.html
+ 
+
